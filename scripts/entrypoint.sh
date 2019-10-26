5da8ce2c4f97a15434af73a825e7c757fc22b622
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2019-10-26 23:57:13 +0800

    fix: file permissions

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,10 +3,17 @@
 set -eo pipefail
 
 main() {
-    rsync -av --ignore-existing /tmp/drupal-7.67/modules /var/www/html/modules
-    rsync -av --ignore-existing /tmp/drupal-7.67/profiles /var/www/html/profiles
-    rsync -av --ignore-existing /tmp/drupal-7.67/sites /var/www/html/sites
-    rsync -av --ignore-existing /tmp/drupal-7.67/themes /var/www/html/themes
+    # fix permissions before copy
+    chown -R www-data.www-data /tmp/drupal-7.67
+
+    # prepare possibly missing files
+    rsync -av --ignore-existing /tmp/drupal-7.67/modules/* /var/www/html/modules
+    rsync -av --ignore-existing /tmp/drupal-7.67/profiles/* /var/www/html/profiles
+    rsync -av --ignore-existing /tmp/drupal-7.67/sites/* /var/www/html/sites
+    rsync -av --ignore-existing /tmp/drupal-7.67/themes/* /var/www/html/themes
+
+    # fix permissions
+    chown -R www-data.www-data /var/www/html
 }
 
 main
